97766867cb2b514c48c3619de7fd1a6f007fafc3
Jericho Rivera <jerichorivera@Jerichos-MacBook-Pro.local>
2022-09-16 15:28:59 +0800

    Modify for docker compose file version 3.9

--- setupenv.sh
+++ setupenv.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ $1 = "start" ]]; then
-    docker-compose up -d
+    docker compose up -d
 
     sleep 60
 
-    for IMG in $(docker-compose ps -a | grep Up | grep replica | awk '{print $1}'); do docker exec -it $IMG bash -c "mysql -uroot -pt00r < /sqlscripts/replication.sql"; done
+    for IMG in $(docker compose ps --status running | jq -r .[].Name); do docker exec -it $IMG bash -c "mysql -uroot -pt00r < /sqlscripts/replication.sql"; done
 
     docker exec -it mysql_source bash -c "mysql -uroot -pt00r < /sqlscripts/user.sql"
 
-    echo "Execute docker-compose logs haproxy1 and check if the backends are up. Alternatively open your browser and browse to <IP>:80/haproxy/stats with user: mysqlstats and pwd: secret"
+    echo "Execute docker compose logs haproxy1 and check if the backends are up. Alternatively open your browser and browse to <IP>:80/haproxy/stats with user: mysqlstats and pwd: secret"
 fi
 
 if [[ $1 = "down" ]]; then
-    docker-compose down
+    docker compose down
 fi
